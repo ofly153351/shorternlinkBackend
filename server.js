1ff1d13a3364a6de6d9d6987d2f7022c525d0a95
oooooooob <peerapatklintan@obx.local>
2025-05-10 22:20:15 +0700

    cors

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors({
     origin: 'https://shorternurlreact-1.onrender.com',
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
